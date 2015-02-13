59b7355c63f1ecafbf8a93f47f27420b8032ac1a
moranned <ned.moran@gmail.com>
2015-02-13 16:52:19 +0000

    Initial commit

---
+++ bartender.py
@@ -0,0 +1,52 @@
+import random
+
+def get_preferences():
+  '''
+  ask customer a series of predefinied questions to determine drink ingredients
+  @param none
+  @return dictionary of customer preferences
+  '''
+  customer_preferences = {}
+  questions = {
+    "strong": "Do ye like yer drinks strong? ",
+    "salty": "Do ye like it with a salty tang? ",
+    "bitter": "Are ye a lubber who likes it bitter? ",
+    "sweet": "Would ye like a bit of sweetness with yer poison? ",
+    "fruity": "Are ye one for a fruity finish? "
+  } 
+  for k,v in questions.items():
+    answer = raw_input(v)
+    if answer.lower() == 'yes' or answer.lower() == 'yes':
+      customer_preferences[k] = True
+    else:
+      customer_preferences[k] = False
+  return customer_preferences   
+
+def make_drink(prefs):
+  '''
+  construct a drink order
+  @param dictionary of customer preferences
+  @return list of order ingredients
+  '''
+  ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+  }
+  order_ingredients = []
+  for k,v in prefs.items():
+    if v is True:
+      order_ingredients.append(random.choice(ingredients.get(k)))
+  return order_ingredients
+
+def main():
+  customer_order = make_drink(get_preferences())
+  print '\nYour drink has the following ingredients:\n',
+  for i in customer_order:
+     print 'a %s ' %i
+  
+  
+if __name__ == '__main__':
+  main()
